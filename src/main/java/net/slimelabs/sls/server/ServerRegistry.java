05ad28510917a936025b39ff720cba44dbd3da1a
Jesse Faler <48862344+jessefaler@users.noreply.github.com>
2025-01-19 12:42:07 -0600

    Fix early return bug in shutdownAllServers()
    Fixed an issue in the shutdownAllServers() method where the function returned prematurely, causing all servers to not shut down as intended.

--- src/main/java/net/slimelabs/sls/server/ServerRegistry.java
+++ src/main/java/net/slimelabs/sls/server/ServerRegistry.java
@@ -4,19 +4,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mattmalec.pterodactyl4j.UtilizationState;
 import com.velocitypowered.api.command.CommandSource;
-import net.slimelabs.sls.server.core.Flags;
+import net.slimelabs.sls.io.ServerData;
+import net.slimelabs.sls.server.core.Server;
 import net.slimelabs.sls.server.core.ServerInstance;
 
 import java.util.HashMap;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ServerRegistry {
 
     // ServerName --> ID
     private final HashMap<String, ServerInstance> servers = new HashMap<>();
-    private final HashMap<String, Flags> savedFlags = new HashMap<>();
 
     /**
      * Starts a server or creates it if it doesn't exist.
@@ -26,8 +24,17 @@ public class ServerRegistry {
      */
     public boolean startServer(String name, ServerConfiguration serverConfiguration) {
         ServerInstance serverInstance = new ServerInstance(name);
+        configureFlags(serverInstance, serverConfiguration);
         registerServer(name, serverInstance);
         return serverInstance.startServer(serverConfiguration);
+    }
+
+    /**
+     * Checks if the server registry is empty.
+     * @return true is there are no servers in the registry
+     */
+    public boolean isRegistryEmpty() {
+        return servers.isEmpty();
     }
 
     /**
@@ -39,9 +46,28 @@ public class ServerRegistry {
      */
     public boolean startServer(String name, ServerConfiguration serverConfiguration, CommandSource source) {
         ServerInstance serverInstance = new ServerInstance(name);
+        configureFlags(serverInstance, serverConfiguration);
         registerServer(name, serverInstance);
         serverInstance.setSource(source);
         return serverInstance.startServer(serverConfiguration);
+    }
+
+    /**
+     * Configures the server flags
+     * Merges any configuration flags with saved flags from the database
+     * @param serverInstance the server instance
+     * @param serverConfiguration the server configuration data
+     */
+    public void configureFlags(ServerInstance serverInstance, ServerConfiguration serverConfiguration) {
+        if(serverInstance.flags != null) {
+            ServerData.insertServerFlags(serverInstance.name, serverInstance.flags.getFlagsAsString());
+        } else {
+            serverInstance.flags = new Flags();
+        }
+        serverInstance.flags.parseFlagsFromString(ServerData.getServerFlags(serverInstance.name)); // Get any saved flags from the database
+        if(serverConfiguration.flags != null) {
+            serverInstance.flags = serverInstance.flags.mergeFlags(serverInstance.flags, serverConfiguration.flags); // Merge the configuration flags with the saved flags
+        }
     }
 
     /**
@@ -49,32 +75,17 @@ public class ServerRegistry {
      * @param serverConfiguration the servers configuration data
      * @param name the name of the server
      * @param source the source that called for the server creation i.e., player
+     * @param flags custom flags to use (overrides existing flags) custom flags are saved in ServerData.db
      * @return true if no errors occurred
      */
     public boolean startServer(String name, ServerConfiguration serverConfiguration, CommandSource source, Flags flags) {
-        flags = configureFlags(name, flags);
-        savedFlags.put(name, flags);
         ServerInstance serverInstance = new ServerInstance(name);
         serverInstance.setFlags(flags);
+        configureFlags(serverInstance, serverConfiguration); // Configure the server flags
         registerServer(name, serverInstance);
         serverInstance.setSource(source);
         return serverInstance.startServer(serverConfiguration);
     }
-
-    public Flags configureFlags(String name, Flags newFlags) {
-        Flags savedFlags = this.savedFlags.get(name);
-        if (savedFlags != null) {
-            // Override saved flags with new flags
-            if (newFlags.SAVE) savedFlags.SAVE = true;
-            if (newFlags.VIEW_DISTANCE != 0) savedFlags.VIEW_DISTANCE = newFlags.VIEW_DISTANCE;
-            if (newFlags.RAM != 0) savedFlags.RAM = newFlags.RAM;
-            if (newFlags.PLAYERS != 0) savedFlags.PLAYERS = newFlags.PLAYERS;
-        } else {
-            savedFlags = newFlags;
-        }
-        return newFlags;
-    }
-
 
     /**
      * Starts a server or creates it if it doesn't exist.
@@ -84,6 +95,7 @@ public class ServerRegistry {
      * @return true if no errors occurred
      */
     public boolean startServer(String name, ServerConfiguration serverConfiguration, CommandSource source, ServerInstance serverInstance) {
+        configureFlags(serverInstance, serverConfiguration);
         registerServer(name, serverInstance);
         serverInstance.setSource(source);
         return serverInstance.startServer(serverConfiguration);
@@ -128,7 +140,7 @@ public class ServerRegistry {
     }
 
     /**
-     * Gets a servers status.
+     * Gets a server status.
      * online, starting, stopped, offline
      * @return the status of the server
      */
@@ -142,6 +154,15 @@ public class ServerRegistry {
 
     public ServerInstance getServer(String name) {
         return servers.get(name);
+    }
+
+    /**
+     * Shutdown all servers present in the server registry.
+     */
+    public void shutDownAllServers() {
+        for(ServerInstance serverInstance : servers.values()) {
+            serverInstance.shutdown();
+        }
     }
 
     /**
