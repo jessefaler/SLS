5d6875a7fbe9c603015887f047f875df56055198
Jesse Faler <48862344+jessefaler@users.noreply.github.com>
2025-01-19 12:58:01 -0600

    Add endpoint urls and keys

--- src/main/java/net/slimelabs/sls/api/Endpoint.java
+++ src/main/java/net/slimelabs/sls/api/Endpoint.java
@@ -1,18 +1,23 @@
 package net.slimelabs.sls.api;
 
+/**
+ * Contains Endpoint Urls and Keys
+ * Warning: This class contains sensitive information do not share contents of this class publicly.
+ */
 public enum Endpoint {
-    APPLICATION_SERVERS("/application/servers"),
-    SERVERS("/servers"),
-    NODES("/application/nodes");
 
-    private static final String BASE_URL = "http://panel.slimelabs.net/api";
-    private final String path;
+    APPLICATION_API_URL("http://panel.slimelabs.net"),                       // Application Api Endpoint
+    CLIENT_API_URL("http://panel.slimelabs.net"),                            // Client Api Endpoint
+    APPLICATION_API_KEY("ptla_A0T0M72ZKXYZd73inutGvT0C8s9U1kn6k3dhGAxIOtT"), // Application Api key
+    CLIENT_API_KEY("ptlc_4fhKAdMgAirrXwcmmQJc836CMc0WM1CdKGh2sfPpvzm");      // Client Api Key
 
-    Endpoint(String path) {
-        this.path = path;
+    private final String value;
+
+    Endpoint(String value) {
+        this.value = value;
     }
 
-    public String getUrl() {
-        return BASE_URL + path;
+    public String getValue() {
+        return value;
     }
 }
