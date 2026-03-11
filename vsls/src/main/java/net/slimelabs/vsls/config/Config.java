8865f0fd8e6209deb884e6c20a70888e1914b1da
jessefaler <protoxon.owner@gmail.com>
2026-03-11 16:29:13 -0500

    chore(vSLS): add snakeYAML dependency and use v2.2 in config loader

--- vsls/src/main/java/net/slimelabs/vsls/config/Config.java
+++ vsls/src/main/java/net/slimelabs/vsls/config/Config.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.LoaderOptions;
 
 public class Config {
 
@@ -48,7 +49,8 @@ public class Config {
         Path targetPath = Path.of(path);
         createDefault(targetPath);
 
-        Yaml yaml = new Yaml(new Constructor(Config.class));
+        LoaderOptions options = new LoaderOptions();
+        Yaml yaml = new Yaml(new Constructor(Config.class, options));
         try (InputStream in = Files.newInputStream(targetPath)) {
             return yaml.load(in);
         } catch (Exception e) {
@@ -85,7 +87,8 @@ public class Config {
             createDefault(targetPath);
         }
 
-        Yaml yaml = new Yaml(new Constructor(Config.class));
+        LoaderOptions options = new LoaderOptions();
+        Yaml yaml = new Yaml(new Constructor(Config.class, options));
         try (InputStream in = Files.newInputStream(targetPath)) {
             SLS.config = yaml.load(in);
             Log.info("Configuration reloaded.");
