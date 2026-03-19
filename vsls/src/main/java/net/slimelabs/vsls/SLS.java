a6e2ab3e395e38f5be16c6818cf1da45db656320
jessefaler <protoxon.owner@gmail.com>
2026-03-19 18:54:39 -0500

    feat(vSLS): add messaging channel for receiving join requests from plugins

--- vsls/src/main/java/net/slimelabs/vsls/SLS.java
+++ vsls/src/main/java/net/slimelabs/vsls/SLS.java
@@ -10,6 +10,7 @@ import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.slimelabs.vsls.blueprints.BlueprintRegistry;
+import net.slimelabs.vsls.channel.MessageChannel;
 import net.slimelabs.vsls.command.SLSCommand;
 import net.slimelabs.vsls.config.Config;
 import net.slimelabs.vsls.events.EventRouter;
@@ -22,6 +23,8 @@ import net.slimelabs.vsls.matchmaking.registry.GameTypeRegistry;
 import net.slimelabs.vsls.matchmaking.strategies.RandomBlueprintStrategy;
 import net.slimelabs.vsls.packets.ChatPackets;
 import net.slimelabs.vsls.server.ServerManager;
+
+import static net.slimelabs.vsls.channel.MessageChannel.SLS_CHANNEL;
 
 @Plugin(
         id = "vsls",
@@ -103,6 +106,11 @@ public class SLS {
         DirectServerJoiner directJoiner = new DirectServerJoiner();
         SLS.joinService = new JoinService(matchmakingManager, directJoiner);
         SLS.api = api;
+
+        // Register the slimelabs network channel
+        SLS.proxy.getChannelRegistrar().register(SLS_CHANNEL);
+        // Register the plugin message listener
+        SLS.proxy.getEventManager().register(this, new MessageChannel.SLSMessageListener());
     }
 
     @Subscribe
